948a31bca17ea0ccd8da18c8f47c0586998c0a96
ShiroSan123 <71055098+ShiroSan123@users.noreply.github.com>
2021-04-25 16:11:27 +0900

    Update site.php

--- site.php
+++ site.php
@@ -5,7 +5,7 @@ $connect = mysqli_connect("127.0.0.1",root,"","MPIT_RLI");
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta charset="utf-8"> 
 	<meta name="viewport" content="width=device-width; initial-scale=1.0">
 	<title></title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
